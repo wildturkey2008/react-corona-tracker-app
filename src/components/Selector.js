9591b9ad9309f66a16d90b12889fd4ef208e35b7
Kazumi Miura <bluesky-dev@aoisora.red>
2023-11-23 21:20:01 +0900

    react-router-dom installed

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -2,7 +2,7 @@
 
 const Selector = (props) => {
   return (
-    <div>
+    <div className="selector-container">
       <select onChange={(e) => props.setCountry(e.target.value)}>
         <option>Select A Country</option>
         {props.countriesJson.map((country, index) => (
--- src/pages/TopPage.js
+++ src/pages/TopPage.js
@@ -5,17 +5,21 @@ import Selector from '../components/Selector';
 import Results from '../components/Results';
 
 const TopPage = (props) => {
-  console.log(props);
   return (
-    <div>
-      <Title />
-      <Selector
-        countriesJson={props.countriesJson}
-        setCountry={props.setCountry}
-        getCountryData={props.getCountryData}
-      />
-      <Results countryData={props.countryData} />
+    <div className="top-page-container">
+      <div>
+        <Title />
+        <Selector
+          countriesJson={props.countriesJson}
+          setCountry={props.setCountry}
+          getCountryData={props.getCountryData}
+        />
+        <Results countryData={props.countryData} />
+      </div>
+
     </div>
+
+
   );
 };
 
